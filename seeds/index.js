b4d31c59a25624b0d9facfde17d8eec5bdf1217a
MESSI-of-coding <messiisgoatinfootball@gmail.com>
2024-08-14 00:16:14 +0530

    Added a review form with features like create, update and delete

--- seeds/index.js
+++ seeds/index.js
@@ -3,37 +3,38 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/tent-tribe', {
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     //useCreateIndex: true,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Database connected');
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
-       const camp = new Campground({
+        const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, consequuntur. Dolore, magni. Accusantium, voluptatibus. Dolore, magni. Accusantium, voluptatibus lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            price 
+            image: 'https://www.pexels.com/photo/1061640/download/?search_query=camping&tracking_id=srh8l89fjd',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
         })
         await camp.save();
     }
-
 }
 
 seedDB().then(() => {
     mongoose.connection.close();
-});
+})
